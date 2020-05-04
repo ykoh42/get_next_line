cf00a0586ba5b700b57ab4729ecc4b922920d84b
ykoh <ykoh@student.42seoul.kr>
2020-05-05 08:19:21 +0900

    Fix typo

--- get_next_line.c
+++ get_next_line.c
@@ -6,82 +6,70 @@
 /*   By: ykoh <ykoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 11:44:46 by ykoh              #+#    #+#             */
-/*   Updated: 2020/05/02 16:39:59 by ykoh             ###   ########.fr       */
+/*   Updated: 2020/05/05 08:18:15 by ykoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <stdio.h>
 #include "get_next_line.h"
-#include "libft.h"
-#define BUFFER_SIZE 1000000000
+#define BUFFER_SIZE 1000
 
-static char	*lineupdate(char *line, char *tmp)
+static char	*ft_parser(char *line, char *buf, char *nl)
 {
-	char	*updated;
+	char	*tmp;
 
-	updated = ft_strjoin(line, tmp);
-	free(line);
-	return (updated);
+	if (nl == NULL)
+		tmp = ft_strjoin(line, buf);
+	else
+	{
+		tmp = ft_strndup(buf, nl - buf + 1);
+		free(line);
+		ft_strlcpy(buf, nl + 1, BUFFER_SIZE);
+	}
+	return (tmp);
 }
 
 int			get_next_line(int fd, char **line)
 {
 	static char	buf[BUFFER_SIZE + 1];
-	char	*nl;
-	char	*tmp;
-	ssize_t	readcnt;
+	ssize_t		readcnt;
+	char		*nl;
 
-	if (!(*line = ft_strdup("")))
+	if (fd < 0 || !line || BUFFER_SIZE <= 0)
 		return (-1);
-	if (ft_strlen(buf))
+	if (!(*line = ft_strndup("", 0)))
+		return (-1);
+	//버퍼가 비었으면
+	while (buf[0] == '\0' && ((readcnt = read(fd, buf, BUFFER_SIZE)) > 0))
 	{
-		while (ft_strlen(buf))
-		{
-			// nl 검사
-			if ((nl = ft_strchr(buf, '\n')))
-			{
-				//nl있으면 붙이고 떙기고 리턴
-				if (!(tmp = ft_substr(buf, 0, nl - buf + 1)))
-						return (-1);
-				if (!(*line = lineupdate(*line, tmp)))
-						return (-1);
-				free(tmp);
-				ft_memcpy(buf, nl + 1, BUFFER_SIZE - (nl - buf));
-				return (1);
-			}
-			else
-			{
-				//nl 없으면 붙이고 루프 반복
-				if (!(*line = lineupdate(*line, buf)))
-					return (-1);
-				buf[0] = '\0';
-			}
-		}
-	}
-	while ((readcnt = read(fd, buf, BUFFER_SIZE)) > 0)
-	{
-		if (readcnt == -1)
-			break ;
 		buf[readcnt] = '\0';
-		// nl 검사
 		if ((nl = ft_strchr(buf, '\n')))
 		{
-			//nl있으면 붙이고 떙기고 리턴
-			if (!(tmp = ft_substr(buf, 0, nl - buf + 1)))
-					return (-1);
-			if (!(*line = lineupdate(*line, tmp)))
-					return (-1);
-			free(tmp);
-			ft_memcpy(buf, nl + 1, BUFFER_SIZE - (nl - buf));
+			if (!(*line = ft_parser(*line, buf, nl)))
+				return (-1);
 			return (1);
 		}
 		else
 		{
-			//nl 없으면 붙이고 루프 반복
-			if (!(*line = lineupdate(*line, buf)))
+			if (!(*line = ft_parser(*line, buf, nl)))
 				return (-1);
 			buf[0] = '\0';
-		}
+		}		
 	}
-	free(*line);
+	//버퍼가 안비었으면
+	while (buf[0] != '\0')
+	{
+		if ((nl = ft_strchr(buf, '\n')))
+		{
+			if (!(*line = ft_parser(*line, buf, nl)))
+				return (-1);
+			return (1);
+		}
+		else
+		{
+			if (!(*line = ft_parser(*line, buf, nl)))
+				return (-1);
+			buf[0] = '\0';
+		}		
+	}
 	return (readcnt);
 }

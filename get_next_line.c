5bb4a008f516fd9ea72d30fc237bb613ae7b829f
ykoh <ykoh@student.42seoul.kr>
2020-05-15 23:42:42 +0900

    Make it observe norminette

--- get_next_line.c
+++ get_next_line.c
@@ -6,42 +6,46 @@
 /*   By: ykoh <ykoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/22 22:01:51 by ykoh              #+#    #+#             */
-/*   Updated: 2020/06/04 20:50:45 by ykoh             ###   ########.fr       */
+/*   Updated: 2020/06/05 13:19:45 by ykoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static int	ft_nl(char **line, char **tmpline)
+{
+	const char	*tmp = *tmpline;
+	const char	*nl = ft_strchr(*tmpline, '\n');
+
+	*line = ft_strndup(tmp, nl - tmp);
+	*tmpline = ft_strndup(nl + 1, BUFFER_SIZE);
+	free((void *)tmp);
+	return (1);
+}
+
 int			get_next_line(int fd, char **line)
 {
-	static char	*tmpbuf;
+	static char	*tmpline;
 	char		buf[BUFFER_SIZE + 1];
 	char		*nl;
 	char		*tmp;
 	ssize_t		readcnt;
 
-	if (fd < 0 || fd > OPEN_MAX || !line || BUFFER_SIZE <= 0)
+	if (!line || BUFFER_SIZE <= 0)
 		return (-1);
-	while (!(nl = ft_strchr(tmpbuf, '\n')) &&
-			(readcnt = read(fd, buf, BUFFER_SIZE)) != 0)
+	while ((!(nl = ft_strchr(tmpline, '\n')) &&
+			(readcnt = read(fd, buf, BUFFER_SIZE)) != 0))
 	{
 		if (readcnt == -1)
 			return (-1);
 		buf[readcnt] = '\0';
-		tmp = (tmpbuf) ? ft_strjoin(tmpbuf, buf) : ft_strndup(buf, readcnt);
-		free(tmpbuf);
-		tmpbuf = tmp;
+		tmp = (tmpline) ? ft_strjoin(tmpline, buf) : ft_strndup(buf, readcnt);
+		free(tmpline);
+		tmpline = tmp;
 	}
 	if (nl)
-	{
-		tmp = tmpbuf;
-		*line = ft_strndup(tmp, nl - tmp);
-		tmpbuf = ft_strndup(nl + 1, BUFFER_SIZE);
-		free(tmp);
-		return (1);
-	}
-	*line = (tmpbuf) ? tmpbuf : ft_strndup("", 1);
-	free(tmpbuf);
-	tmpbuf = NULL;
+		return (ft_nl(line, &tmpline));
+	*line = (tmpline) ? tmpline : ft_strndup("", 1);
+	tmpline = NULL;
 	return (0);
 }
